0fcfad5d719c2a05979baca35fd2b716fa556bd4
Gabriel Batistuta <batistutag190@gmail.com>
2022-11-07 16:21:24 -0300

    Formatação de Strings e quebras de linhas

--- src/curso_java/Main.java
+++ src/curso_java/Main.java
@@ -1,10 +1,35 @@
 package curso_java;
 
+import java.util.Locale;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("KA-ME-HA-ME-HAAAAAAAAAAA!");
-
+		Locale.setDefault(Locale.US);
+//		int y = 32;
+//		System.out.print(y);
+//		
+//		System.out.print(" + ");
+//		
+//		double x = 10.35784;
+//		double z = y + x;
+//		System.out.printf("%.2f = %.2f%n", x,z);
+//		
+		System.out.println("Jack-chan luta até hoje");
+		System.out.println("KA-ME-HA-ME-HAAAAAAAAAAA!\n");
+		
+		String product1 = "Computer";
+		String product2 = "Office desktop";
+		
+		int age = 30;
+		int code = 5290;
+		char gender = 'F';
+		
+		double price1 = 2100.0;
+		double price2 = 650.50;
+		double measure = 53.234567;
+		
+		System.out.println("Products:");
 	}
 
 }
